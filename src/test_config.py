43f053fe5bb789a2833b88a0b500646a8bfcae90
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2017-01-18 23:15:49 +0100

    added tests for loglevel and retention validation

--- src/test_config.py
+++ src/test_config.py
@@ -33,13 +33,47 @@ def test_valid_compression_level(config):
 def test_invalid_compression_algorithm(config):
 
     for algorithm in ["lz6", "lzma3"]:
+
         cfg['compression'] = algorithm
+
         with pytest.raises(ConfigValidationError) as e:
             validate_config(cfg)
             assert e == "Invalid compression algorithm {}".format(cfg['compression_level'])
 
-def test_valid_compression_algorithm(config):
+
+def test_compression_algorithm(config):
 
     for algorithm in ['none', 'lz4', 'zlib', 'lzma']:
         cfg['compression'] = algorithm
+        assert validate_config(cfg) == True
+
+
+def test_invalid_loglevels(config):
+
+    for level in ['INFORMATIONAL', 'DANGEROUS', 'SOMEVALUE']:
+
+        cfg['logging']['level'] = level
+
+        with pytest.raises(ConfigValidationError) as e:
+            validate_config(cfg)
+            assert e == 'Invalid log level "{}" specified'.format(cfg['logging']['level'])
+
+
+def test_valid_loglevels(config):
+
+    for level in ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']:
+        cfg['logging']['level'] = level
+        assert validate_config(cfg) == True
+
+
+
+def test_valid_retention(config):
+
+    valid_retention_configurations = [
+        {'enable': True, 'hourly': 1, 'daily': 1, 'weekly': 1, 'monthly': 1, 'yearly': 1},
+        {'enable': True, 'hourly': 10, 'daily': 20, 'weekly': 30, 'monthly': 40, 'yearly': 50}
+    ]
+
+    for retention in valid_retention_configurations:
+        cfg['retention'] = retention
         assert validate_config(cfg) == True
